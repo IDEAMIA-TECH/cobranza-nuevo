da4ebba47a4292784885d0d4a9705300f7d740cd
IDEAMIA-TECH <155779384+IDEAMIA-TECH@users.noreply.github.com>
2025-02-21 16:18:34 -0600

    email

--- includes/Mailer.php
+++ includes/Mailer.php
@@ -4,6 +4,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/Settings.php';
 
 class Mailer {
     private $mail;
